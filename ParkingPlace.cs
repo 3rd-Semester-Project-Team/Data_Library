2d36f805d83b0b09be9c3c7ab0e9dd7ccb59dfd6
Paulina-21 <73336378+Paulina-21@users.noreply.github.com>
2021-11-23 09:47:29 +0100

    Add files via upload

---
+++ ParkingPlace.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Data_Library
+{
+    /// <summary>
+    /// Defines the info of a parking slot
+    /// </summary>
+    public class ParkingPlace
+    {
+        /// <summary>
+        /// Id of the sensor
+        /// </summary>
+        public int SensorId { get; set; }
+        /// <summary>
+        /// Id of the Parking slot
+        /// </summary>
+        public int ParkingId { get; set; }
+        /// <summary>
+        /// Time when the state of Occupied (boolean) is changed
+        /// </summary>
+        public DateTime SensorDateTime { get; set; }
+        /// <summary>
+        /// States if a parking slot is taken or not
+        /// </summary>
+        public bool Occupied { get; set; }
+    }
+}
